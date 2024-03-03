950c8251da520504f9283eb58af597539af69b1b
Noel KOUASSI <noel.kouassi@outlook.com>
2024-03-03 14:56:05 +0100

    Mise en place des tests unitaires concernant le composant app (AppComponent)

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,14 +1,35 @@
-import { HttpClientModule } from '@angular/common/http';
-import { TestBed } from '@angular/core/testing';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterTestingModule } from '@angular/router/testing';
-import { expect } from '@jest/globals';
+import {HttpClientModule} from '@angular/common/http';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterTestingModule} from '@angular/router/testing';
+import {expect} from '@jest/globals';
 
-import { AppComponent } from './app.component';
-
+import {AppComponent} from './app.component';
+import {Router} from "@angular/router";
+import {SessionService} from "./services/session.service";
+import {of} from "rxjs";
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let routerMock: jest.Mocked<Router>;
+  let sessionServiceMock: jest.Mocked<SessionService>;
+
   beforeEach(async () => {
+
+    routerMock = {
+      navigate: jest.fn() as jest.MockedFunction<typeof routerMock.navigate>
+    } as jest.Mocked<Router>;
+
+    sessionServiceMock = {
+      sessionInformation: {
+        admin: true,
+        id: 1
+      },
+      $isLogged: jest.fn() as jest.MockedFunction<typeof sessionServiceMock.$isLogged>,
+      logOut: jest.fn() as jest.MockedFunction<typeof sessionServiceMock.logOut>
+    } as jest.Mocked<SessionService>;
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -18,12 +39,42 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        {provide: Router, useValue: routerMock},
+        {provide: SessionService, useValue: sessionServiceMock}
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should return user connection on log in', () => {
+
+    // Given
+    const isLoggedMockValue = true;
+
+    // When
+    sessionServiceMock.$isLogged.mockReturnValue(of(isLoggedMockValue));
+
+    // Then
+    component.$isLogged().subscribe((result) => {
+      expect(result).toEqual(isLoggedMockValue);
+      expect(sessionServiceMock.$isLogged).toHaveBeenCalled();
+    });
+  });
+
+  it('should log out and navigate to home', () => {
+
+    // When
+    component.logout();
+
+    // Then
+    expect(sessionServiceMock.logOut).toHaveBeenCalled();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['']);
   });
 });
