e8398f6eb81179c8cacec2ea010b9fd37062cb92
Noel KOUASSI <noel.kouassi@outlook.com>
2024-03-09 20:03:23 +0100

    Mise en place des tests d'integration du composant list

--- front/src/app/features/sessions/components/list/list.component.spec.ts
+++ front/src/app/features/sessions/components/list/list.component.spec.ts
@@ -1,5 +1,5 @@
 import {HttpClientModule} from '@angular/common/http';
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, waitForAsync} from '@angular/core/testing';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {expect} from '@jest/globals';
@@ -9,18 +9,22 @@ import {ListComponent} from './list.component';
 import {SessionApiService} from "../../services/session-api.service";
 import {of} from "rxjs";
 import {Session} from "../../interfaces/session.interface";
+import {Router} from "@angular/router";
+import {SessionInformation} from "../../../../interfaces/sessionInformation.interface";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
   let sessionServiceMock: jest.Mocked<SessionService>;
   let sessionApiServiceMock: jest.Mocked<SessionApiService>;
+  let routerMock: jest.Mocked<Router>;
 
   const mockSessionService = {
     sessionInformation: {
       admin: true
     }
-  }
+  };
 
   const mockSessionList = [{id: 1}] as Session[];
 
@@ -31,10 +35,14 @@ describe('ListComponent', () => {
       all: jest.fn() as jest.MockedFunction<typeof sessionApiServiceMock.all>,
     } as jest.Mocked<SessionApiService>
 
+    routerMock = {
+      navigate: jest.fn() as jest.MockedFunction<typeof routerMock.navigate>
+    } as jest.Mocked<Router>;
+
     await TestBed.configureTestingModule({
       declarations: [ListComponent],
-      imports: [HttpClientModule, MatCardModule, MatIconModule],
-      providers: [{provide: SessionService, useValue: mockSessionService},
+      imports: [HttpClientModule, MatCardModule, MatIconModule, RouterTestingModule],
+      providers: [
         {provide: SessionService, useValue: sessionServiceMock},
         {provide: SessionApiService, useValue: sessionApiServiceMock}
       ]
@@ -50,6 +58,10 @@ describe('ListComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should get user data', () => {
+    expect(component.user).toEqual(sessionServiceMock.sessionInformation);
+  });
+
   it('should get all sessions on start', waitForAsync(() => {
 
     // When
@@ -62,7 +74,29 @@ describe('ListComponent', () => {
     });
   }));
 
-  it('should get user data', () => {
-    expect(component.user).toEqual(sessionServiceMock.sessionInformation);
+  it('should show create button for admin users', () => {
+
+    // When
+    fixture.detectChanges();
+    const createButton = fixture.nativeElement.querySelector('button[routerLink="create"]');
+
+    // Then
+    expect(createButton).toBeTruthy();
   });
+
+  it('should not show create button for non-admin users', () => {
+
+    // Given
+    sessionServiceMock.sessionInformation = {
+      admin: false
+    } as SessionInformation;
+
+    // When
+    fixture.detectChanges();
+    const createButton = fixture.nativeElement.querySelector('button[routerLink="create"]');
+
+    // Then
+    expect(createButton).toBeFalsy();
+  });
+
 });
